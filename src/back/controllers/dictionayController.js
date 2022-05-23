070433260ca1e6caadbd03696efb095052cea11e
William <webuntermench@protonmail.com>
2022-05-23 15:00:43 +0300

    controllers for the app

---
+++ src/back/controllers/dictionayController.js
@@ -0,0 +1,27 @@
+const getAllWords = (req, res) => {
+  return;
+};
+
+const getOneWord = (req, res) => {
+  return;
+};
+
+const createNewWord = (req, res) => {
+  return;
+};
+
+const updateOneWord = (req, res) => {
+  return;
+};
+
+const deleteOneWord = (req, res) => {
+  return;
+};
+
+module.exports = {
+  getAllWords,
+  getOneWord,
+  createNewWord,
+  updateOneWord,
+  deleteOneWord,
+};
