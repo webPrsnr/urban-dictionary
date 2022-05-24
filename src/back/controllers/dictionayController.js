d821ce7231cdbe58c925b392b54325ca877ca0cf
William <webuntermench@protonmail.com>
2022-05-24 19:37:32 +0300

    the function of adding a new word

--- src/back/controllers/dictionayController.js
+++ src/back/controllers/dictionayController.js
@@ -16,8 +16,40 @@ const getOneWord = (req, res) => {
   return;
 };
 
-const createNewWord = (req, res) => {
-  return;
+const createNewWord = async (req, res) => {
+  const { body } = req;
+  if (
+    !body.word_name ||
+    !body.transcription ||
+    !body.mean ||
+    !body.description ||
+    !body.created_at ||
+    !body.updated_at
+  ) {
+    res.status(400).send({
+      status: "FAILED",
+      data: {
+        error:
+          "One of the following keys is missing or is empty in request body: 'word_name' 'transcription' 'mean' 'description' 'created_at' 'updated_at'",
+      },
+    });
+  }
+  const newWord = {
+    word_name: body.word_name,
+    transcription: body.transcription,
+    mean: body.mean,
+    description: body.description,
+  };
+
+  try {
+    const createdWord = await dictionaryService.createNewWord(newWord);
+    res.status(200).send({ status: "OK", data: createdWord });
+  } catch (error) {
+    res.status(error?.status || 500).send({
+      status: "FAILED",
+      data: { error: error?.message || error },
+    });
+  }
 };
 
 const updateOneWord = (req, res) => {
