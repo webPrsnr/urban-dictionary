eb60960521fe143b7beff3c0138ff3a03e4f4e0b
William <webuntermench@protonmail.com>
2022-05-25 21:10:28 +0300

    remove try/catch construction from getAllWords func

--- src/back/database/utils.js
+++ src/back/database/utils.js
@@ -2,23 +2,16 @@ const db = require("../../../db_init");
 
 const getAllWords = () => {
   return new Promise((res, rej) => {
-    try {
-      const GET_ALL_WORDS = `SELECT * FROM words`;
-      db.all(GET_ALL_WORDS, (err, rows) => {
-        if (err) {
-          throw {
-            status: 500,
-            message: err,
-          };
-        }
-        res(rows);
-      });
-    } catch (err) {
-      throw {
-        status: 500,
-        message: err,
-      };
-    }
+    const GET_ALL_WORDS = `SELECT * FROM wosrds`;
+    db.all(GET_ALL_WORDS, (err, rows) => {
+      if (err) {
+        rej({
+          status: 500,
+          message: err,
+        });
+      }
+      res(rows);
+    });
   });
 };
 
