a4871fd1b6478332901089f7df89cfddf91bfae8
William <webuntermench@protonmail.com>
2022-05-30 15:34:38 +0300

    basic routing using react-router-dom

--- src/front/components/App/App.js
+++ src/front/components/App/App.js
@@ -1,11 +1,21 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "../Layout/Layout";
+import Home from "../../pages/Home/Home";
+import Words from "../../pages/Words/Words";
+import BrowseWords from "../../pages/BrowseWords/BrowseWords";
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello world</h1>
-      <h2>How are you?</h2>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="words" element={<Words />} />
+          <Route path="browse" element={<BrowseWords />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 };
 
