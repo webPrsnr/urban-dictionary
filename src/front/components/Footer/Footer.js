4460d011ba40755148ed190589d4fb38c3fa5786
William <webuntermench@protonmail.com>
2022-05-31 15:52:20 +0300

    footer styles

--- src/front/components/Footer/Footer.js
+++ src/front/components/Footer/Footer.js
@@ -1,8 +1,14 @@
 import React from "react";
+import styles from "./Footer.module.scss";
+
 const Footer = () => {
   return (
     <>
-      <footer>made with love</footer>
+      <footer>
+        made with ❤️ by <a href="https://github.com/webPrsnr">webPrsnr</a>{" "}
+        <br />
+        2022
+      </footer>
     </>
   );
 };
