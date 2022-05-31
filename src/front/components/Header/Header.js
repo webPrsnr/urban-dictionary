08bb0769a89d41cb6c08da8af1e799709e2bbb85
William <webuntermench@protonmail.com>
2022-05-31 14:13:29 +0300

    header styles

--- src/front/components/Header/Header.js
+++ src/front/components/Header/Header.js
@@ -1,13 +1,36 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styles from "./Header.module.scss";
 
 const Header = () => {
   return (
     <>
-      <header>
-        <Link to="/">Home</Link>
-        <Link to="/browse">Browse</Link>
-        <Link to="/words">Words</Link>
+      <header className={styles.header_outer_block}>
+        <div className={styles.header_inner_block}>
+          <div className={styles.header__logo}>
+            <span className={styles.header__logo_title}>urb\an</span>
+            <span className={styles.header__logo_subtitle}>dictionary</span>
+          </div>
+          <div className={styles.header__nav}>
+            <ul className={styles.header__list}>
+              <li className={styles.header__list_el}>
+                <Link className={styles.header__link} to="/">
+                  Home
+                </Link>
+              </li>
+              <li className={styles.header__list_el}>
+                <Link className={styles.header__link} to="/browse">
+                  Browse
+                </Link>
+              </li>
+              <li className={styles.header__list_el}>
+                <Link className={styles.header__link} to="/words">
+                  Words
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
       </header>
     </>
   );
