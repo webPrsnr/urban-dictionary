0c6f1c3a790b2efc6ee14b38d52ee8e7f0e2150d
William <webuntermench@protonmail.com>
2022-05-31 14:14:39 +0300

    basic styles for layout

--- src/front/components/Layout/Layout.js
+++ src/front/components/Layout/Layout.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
+import styles from "./Layout.module.scss";
 
 const Layout = () => {
   return (
