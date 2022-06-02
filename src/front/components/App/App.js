097a249dcc351235eda2e78f55d49f486563f873
William <webuntermench@protonmail.com>
2022-06-02 21:41:46 +0300

    route for :alphabet

--- src/front/components/App/App.js
+++ src/front/components/App/App.js
@@ -4,6 +4,7 @@ import Layout from "../Layout/Layout";
 import Home from "../../pages/Home/Home";
 import Words from "../../pages/Words/Words";
 import BrowseWords from "../../pages/BrowseWords/BrowseWords";
+import { Alphabet } from "../../pages/Alphabet/Alphabet";
 
 const App = () => {
   return (
@@ -12,7 +13,8 @@ const App = () => {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="words" element={<Words />} />
-          <Route path="browse" element={<BrowseWords />} />
+          <Route path="browse" element={<BrowseWords />}></Route>
+          <Route path="browse/:alphabet" element={<Alphabet />} />
         </Route>
       </Routes>
     </BrowserRouter>
