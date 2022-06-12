a30073c6196b9893b476d61e90e8124a2878a0ea
William <webuntermench@protonmail.com>
2022-06-12 23:27:44 +0300

    refactoring of interaction with db

--- src/back/database/Dictionary.js
+++ src/back/database/Dictionary.js
@@ -1,9 +1,22 @@
 const DbUtils = require("./utils");
+const queries = require("./queries");
 
 const getAllWords = async (filterParam) => {
+  const { alphabet, _offset } = filterParam;
   try {
-    const allWords = await DbUtils.getAllWords(filterParam);
-    return allWords;
+    if (alphabet && _offset) {
+      const wordsByLetter = await DbUtils.dbAll(queries.GET_WORDS_BY, [
+        alphabet + "%",
+      ]);
+      const allWords = await DbUtils.dbAll(queries.GET_WORDS_LIMIT, [
+        alphabet + "%",
+        12,
+        _offset,
+      ]);
+      return { allWords, wordsByLetter };
+    }
+    const allWords = await DbUtils.dbAll(queries.GET_ALL_WORDS);
+    return { allWords, wordsByLetter: allWords };
   } catch (err) {
     throw err;
   }
@@ -11,16 +24,24 @@ const getAllWords = async (filterParam) => {
 
 const createNewWord = async (newWord) => {
   try {
-    const insertWord = await DbUtils.createNewWord(newWord);
+    const insertWord = await DbUtils.dbRun(queries.INSERT_WORD, [
+      newWord.id,
+      newWord.word_name,
+      newWord.transcription,
+      newWord.mean,
+      newWord.description,
+      newWord.created_at,
+      newWord.updated_at,
+    ]);
     return insertWord;
   } catch (err) {
     throw err;
   }
 };
 
-const getOneWord = (wordID) => {
+const getOneWord = async (wordID) => {
   try {
-    const resultWord = DbUtils.getOneWord(wordID);
+    const resultWord = await DbUtils.dbAll(queries.GET_WORD, wordID);
     return resultWord;
   } catch (err) {
     throw err;
@@ -29,7 +50,15 @@ const getOneWord = (wordID) => {
 
 const updateOneWord = async (id, changes) => {
   try {
-    const updatedWord = await DbUtils.updateOneWord(id, changes);
+    const updatedWord = await DbUtils.dbRun(queries.UPDATE_WORD, [
+      changes.word_name,
+      changes.transcription,
+      changes.mean,
+      changes.description,
+      changes.created_at,
+      changes.updated_at,
+      id,
+    ]);
     return updatedWord;
   } catch (err) {
     throw err;
@@ -38,7 +67,7 @@ const updateOneWord = async (id, changes) => {
 
 const deleteOneWord = async (id) => {
   try {
-    const changes = await DbUtils.deleteOneWord(id);
+    const changes = await DbUtils.dbRun(queries.DELETE_WORD, id);
     return changes;
   } catch (err) {
     throw err;
