0da86474344f40f8e755daab91dc8d43079961b8
William <webuntermench@protonmail.com>
2022-06-16 16:00:30 +0300

    new query string

--- src/back/database/queries.js
+++ src/back/database/queries.js
@@ -1,5 +1,5 @@
 const GET_ALL_WORDS = `SELECT * FROM words`;
-const GET_ALL_WORDS_LIMIT = `SELECT * FROM words LIMIT ? OFFSET ?`;
+const GET_ALL_WORDS_LIMIT_ORDER = `SELECT * FROM words ORDER BY word_name DESC LIMIT ? OFFSET ?`;
 const GET_WORDS_LIMIT = `SELECT * FROM words WHERE word_name LIKE ? LIMIT ? OFFSET ?`;
 const GET_WORDS_BY = `SELECT * FROM words WHERE word_name LIKE ?`;
 const INSERT_WORD = `INSERT INTO words(id, word_name, transcription, mean, description, created_at, updated_at) VALUES(?, ?, ?, ?, ?, ?, ?)`;
@@ -17,7 +17,7 @@ const DELETE_WORD = "DELETE FROM words WHERE id= ? ";
 
 module.exports = {
   GET_ALL_WORDS,
-  GET_ALL_WORDS_LIMIT,
+  GET_ALL_WORDS_LIMIT_ORDER,
   GET_WORDS_LIMIT,
   GET_WORDS_BY,
   INSERT_WORD,
