75b60f0faaa7bfd1965783161a3f530ea6b18d52
William <webuntermench@protonmail.com>
2022-06-16 16:04:05 +0300

    Sort component

---
+++ src/front/components/Sort/Sort.js
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import styles from "./Sort.module.scss";
+
+const Sort = ({ setSortFlag }) => {
+  const [sortPop, setSortPop] = useState(false);
+  const [sortTitle, setSortTitle] = useState("алфавиту (INC)");
+
+  const showPopup = () => {
+    setSortPop(!sortPop);
+  };
+
+  const list = [
+    { title: "алфавиту (INC)", req: "alphab_inc" },
+    { title: "алфавиту (DEC)", req: "alphab_dec" },
+    { title: "дате (NEW)", req: "date_new" },
+    { title: "дате (OLD)", req: "date_old" },
+  ];
+
+  const changeFlag = (item) => {
+    setSortFlag(item.req);
+    setSortTitle(item.title);
+    showPopup();
+  };
+
+  return (
+    <div className={styles.sort}>
+      <div className={styles.sort__label}>
+        <p>Сортировка по:</p>
+        <span className={styles.sort__by} onClick={showPopup}>
+          {sortTitle}
+        </span>
+      </div>
+      {sortPop && (
+        <>
+          <Backdrop setPop={showPopup} />
+          <div className={styles.sort__popup}>
+            <ul className={styles.sort__list}>
+              {list.map((item, index) => (
+                <li key={index} onClick={() => changeFlag(item)}>
+                  {item.title}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+const Backdrop = ({ setPop }) => {
+  return <div className={styles.backdrop} onClick={setPop}></div>;
+};
+
+export default Sort;
--- src/front/pages/Words/Words.js
+++ src/front/pages/Words/Words.js
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from "react";
 import LetterList from "../../components/LetterList/LetterList";
 import Pagination from "../../components/Pagination/Pagination";
+import Sort from "../../components/Sort/Sort";
 import styles from "./Words.module.scss";
 
 const Words = () => {
   const [words, setWords] = useState([]);
   const [pageCount, setPageCount] = useState(0);
   const [itemOffset, setItemOffset] = useState(0);
+  const [sortFlag, setSortFlag] = useState("alphab_inc");
 
   const handlePageClick = (event) => {
     const newOffset = event.selected * 12;
@@ -16,19 +18,22 @@ const Words = () => {
   useEffect(() => {
     (async function fetchData() {
       const response = await fetch(
-        `http://localhost:8080/api/v1/words/?_offset=${itemOffset}`
+        `http://localhost:8080/api/v1/words/?sort=${sortFlag}&_offset=${itemOffset}`
       );
       const json = await response.json();
       setWords(json.data);
 
       setPageCount(Math.ceil(response.headers.get("x-content") / 12));
     })();
-  }, [itemOffset]);
+  }, [itemOffset, sortFlag]);
 
   return (
     <div className={styles.wrapper_outer}>
       <div className={styles.wrapper_inner}>
-        <h1 className={styles.letters_block}>Все слова</h1>
+        <div className={styles.head_container}>
+          <h1 className={styles.letters_block}>Все слова</h1>
+          <Sort setSortFlag={setSortFlag} />
+        </div>
         <LetterList letters={words} />
         <Pagination handlePageClick={handlePageClick} pageCount={pageCount} />
       </div>
