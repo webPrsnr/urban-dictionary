1ac09d8835f4de9aa37f909ee5348cdfef031300
William <webuntermench@protonmail.com>
2022-06-20 22:32:15 +0300

    API.delete implementation

--- src/front/api/api.js
+++ src/front/api/api.js
@@ -1,7 +1,11 @@
 const CURRENT_URL = `http://localhost:8080/api/v1/`;
 
 export const API = {
-  delete() {},
+  async delete(query) {
+    const response = await fetch(CURRENT_URL + query, { method: "DELETE" });
+    const record = await response.json();
+    return record;
+  },
   async get(query, par) {
     const response = await fetch(CURRENT_URL + query);
     const record = await response.json();
