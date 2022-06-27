af396b244294f5144c04e6485c4c70a2a6d567ef
William <webuntermench@protonmail.com>
2022-06-27 17:26:03 +0300

    redux provider

--- src/front/App/App.js
+++ src/front/App/App.js
@@ -6,19 +6,26 @@ import Words from "../pages/Words/Words";
 // import BrowseWords from "../pages/BrowseWords/BrowseWords";
 import { Alphabet } from "../pages/Alphabet/Alphabet";
 import WordPage from "../pages/WordPage/WordPage";
+import { Provider } from "react-redux";
+import store from "../store/index";
+import "./App.module.scss";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="words" element={<Words />} />
-          <Route path="words/:id" element={<WordPage />} />
-          <Route path="browse/:alphabet" element={<Alphabet />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Home />} />
+              <Route path="words" element={<Words />} />
+              <Route path="words/:id" element={<WordPage />} />
+              <Route path="browse/:alphabet" element={<Alphabet />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
+    </>
   );
 };
 
