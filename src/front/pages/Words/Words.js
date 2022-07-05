ad2763a9bb1c6ddbfcfb023b6a41a13173be5005
William <webuntermench@protonmail.com>
2022-07-05 14:04:45 +0300

    add AddWord component to the Words component

--- src/front/pages/Words/Words.js
+++ src/front/pages/Words/Words.js
@@ -4,13 +4,20 @@ import LetterList from "../../components/LetterList/LetterList";
 import Pagination from "../../components/Pagination/Pagination";
 import Sort from "../../components/Sort/Sort";
 import Search from "../../components/Search/Search";
+import Modal from "../../components/Modal/Modal";
 import styles from "./Words.module.scss";
+import AddWord from "../../components/AddWord/AddWord";
 
 const Words = () => {
   const [words, setWords] = useState([]);
   const [pageCount, setPageCount] = useState(0);
   const [itemOffset, setItemOffset] = useState(0);
   const [sortFlag, setSortFlag] = useState("alphab_inc");
+  const [modalFlag, setModalFlag] = useState(false);
+
+  const modalSwitch = (status) => {
+    setModalFlag(true);
+  };
 
   const handlePageClick = (event) => {
     const newOffset = event.selected * 12;
@@ -29,17 +36,25 @@ const Words = () => {
   }, [itemOffset, sortFlag]);
 
   return (
-    <div className={styles.wrapper_outer}>
-      <div className={styles.wrapper_inner}>
-        <Search />
-        <div className={styles.head_container}>
-          <h1 className={styles.letters_block}>Все слова</h1>
-          <Sort setSortFlag={setSortFlag} />
+    <>
+      <div className={styles.wrapper_outer}>
+        <div className={styles.wrapper_inner}>
+          <Search />
+          <div className={styles.head_container}>
+            <h1 className={styles.letters_block}>Все слова</h1>
+            <div className={styles.button} onClick={modalSwitch}>
+              Добавить+
+            </div>
+            <Sort setSortFlag={setSortFlag} />
+          </div>
+          <LetterList letters={words} />
+          <Pagination handlePageClick={handlePageClick} pageCount={pageCount} />
         </div>
-        <LetterList letters={words} />
-        <Pagination handlePageClick={handlePageClick} pageCount={pageCount} />
       </div>
-    </div>
+      <Modal modalFlag={modalFlag} setModalFlag={setModalFlag}>
+        <AddWord />
+      </Modal>
+    </>
   );
 };
 
