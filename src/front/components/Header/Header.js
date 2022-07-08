08c82472f6f07bca64a437c9a73733560af29017
William <webuntermench@protonmail.com>
2022-07-08 17:09:02 +0300

    remove drop down from component

--- src/front/components/Header/Header.js
+++ src/front/components/Header/Header.js
@@ -1,35 +1,13 @@
 import React from "react";
 import { NavLink, Link } from "react-router-dom";
 import styles from "./Header.module.scss";
-import { useState } from "react";
-import alphabet from "./alphabet";
 import Theme from "../Theme/Theme";
+import LettersDropDown from "../LettersDropDown/LettersDropDown";
 
 const Header = () => {
-  const [dropDownFlag, setDropDownFlag] = useState(false);
-
-  const showDropDown = () => {
-    setDropDownFlag(true);
-  };
-
-  const hideDropDown = () => {
-    setDropDownFlag(false);
-  };
-
   const setActive = ({ isActive }) =>
     isActive ? styles.active_link : styles.header__link;
 
-  const dropDown = (
-    <div className={styles.header__dropdown} onMouseEnter={showDropDown}>
-      <ul className={styles.header__alphabet}>
-        {alphabet.map((letter, index) => (
-          <Link key={index} to={`/browse/${letter}`}>
-            <li className={styles.alphabet__link}>{letter}</li>
-          </Link>
-        ))}
-      </ul>
-    </div>
-  );
   return (
     <>
       <header className={styles.header_outer_block}>
@@ -45,13 +23,8 @@ const Header = () => {
                   Home
                 </NavLink>
               </li>
-              <li
-                className={styles.header__list_el}
-                onMouseEnter={showDropDown}
-                onMouseLeave={hideDropDown}
-              >
-                Browse <span className={styles.browser_link}></span>
-                {dropDownFlag ? dropDown : null}
+              <li className={styles.header__list_el}>
+                <LettersDropDown />
               </li>
               <li className={styles.header__list_el}>
                 <NavLink className={setActive} to="/words">
