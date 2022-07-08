84038e6992345a665009bbb09926625de001416a
William <webuntermench@protonmail.com>
2022-07-08 12:23:42 +0300

    react lazy

--- src/front/App/App.js
+++ src/front/App/App.js
@@ -1,27 +1,31 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "../components/Layout/Layout";
-import Home from "../pages/Home/Home";
-import Words from "../pages/Words/Words";
-import { Alphabet } from "../pages/Alphabet/Alphabet";
-import WordPage from "../pages/WordPage/WordPage";
 import { Provider } from "react-redux";
 import store from "../store/index";
+import Loader from "../components/Loader/Loader";
 import "./App.module.scss";
+
+const Home = lazy(() => import("../pages/Home/Home"));
+const Words = lazy(() => import("../pages/Words/Words"));
+const Alphabet = lazy(() => import("../pages/Alphabet/Alphabet"));
+const WordPage = lazy(() => import("../pages/WordPage/WordPage"));
+const Layout = lazy(() => import("../components/Layout/Layout"));
 
 const App = () => {
   return (
     <>
       <Provider store={store}>
         <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Layout />}>
-              <Route index element={<Home />} />
-              <Route path="words" element={<Words />} />
-              <Route path="words/:id" element={<WordPage />} />
-              <Route path="browse/:alphabet" element={<Alphabet />} />
-            </Route>
-          </Routes>
+          <Suspense fallback={<Loader />}>
+            <Routes>
+              <Route path="/" element={<Layout />}>
+                <Route index element={<Home />} />
+                <Route path="words" element={<Words />} />
+                <Route path="words/:id" element={<WordPage />} />
+                <Route path="browse/:alphabet" element={<Alphabet />} />
+              </Route>
+            </Routes>
+          </Suspense>
         </BrowserRouter>
       </Provider>
     </>
